e479e56df281cd664b931e28240a2a8ec2c35324
arqdsis <arqdsis@MOOLIi0129.animaedu.intranet>
2020-03-10 22:14:15 -0300

    SomaComprimento

--- SomaComprimento.java
+++ SomaComprimento.java
@@ -3,8 +3,12 @@ import javax.swing.JOptionPane;
 public class SomaComprimento{
    public static void main (String [] args){
       String palavra1 = JOptionPane.showInputDialog("Digite uma palavra");
+      String palavra2 = JOptionPane.showInputDialog("Digite uma palavra");
+      String palavra3 = JOptionPane.showInputDialog("Digite uma palavra");
+      String palavra4 = JOptionPane.showInputDialog("Digite uma palavra");
       
-      int comprimento = palavra1.length();
-      JOptionPane.showMessageDialog(null, "O comprimento da sua String é " + comprimento);
+      int somaComprimento = palavra1.length() + palavra2.length() + palavra3.length() + palavra4.length();
+      
+      JOptionPane.showMessageDialog(null, "O comprimento da sua String é " + somaComprimento);
    }
 }
