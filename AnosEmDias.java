878f5ae88d7df0152b67f29783ba598a5ecda149
arqdsis <arqdsis@MOOLIi0129.animaedu.intranet>
2020-03-10 22:03:37 -0300

    AnosEmDias

---
+++ AnosEmDias.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class AnosEmDias{
+   public static void main (String [] args){
+   
+   int anos = Integer.parseInt(JOptionPane.showInputDialog("Quantos anos você tem?"));
+   int calculaDias = anos * 365;
+   
+   JOptionPane.showMessageDialog(null, "Você viveu um total de " + calculaDias + " dias");
+   
+   }
+}
